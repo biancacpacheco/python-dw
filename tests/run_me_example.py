d75de83db66149819b03abe128aacbf27c2617ae
João Arthur <joaoarthurbm@gmail.com>
2017-08-15 09:54:46 -0300

    organizing packages

--- tests/run_me_example.py
+++ tests/run_me_example.py
@@ -1,13 +1,13 @@
-import python_dw
+import api.design_wizard as dw
 
-dw = python_dw.PythonDW()
+dw = dw.PythonDW()
 
 print "PRINTING INITIAL AST TREE: " + str(dw.ast_tree) + "\n"
 
 print "PRINTING INITIAL AST ELEMENTS TO LOOK FOR INTO THE TREE: " + str(dw.ast_elements_dict) + "\n"
 
-print "PARSING FILE: 'test_cases_modules/simple_module.py' \n"
-dw.parse("test_cases_modules/simple_module.py")
+print "PARSING FILE: 'tests/data/simple_module.py' \n"
+dw.parse("tests/data/simple_module.py")
 
 print "GETTING ALL CLASSES: " 
 dw.get_all_classes()
